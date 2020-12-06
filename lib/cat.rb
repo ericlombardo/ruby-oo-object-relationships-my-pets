d6d94a9d10184c7d20a8a82663238ed341fef1cc
Eric <eric.m.lombardo@gmail.com>
2020-12-05 19:37:59 -0500

    commit tree 32830da959411adff15aa0c33fcdd95999c261b3

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood # set and get methods
+  attr_reader :name # just get method, can't be changed
+
+  @@all = []  
+  def initialize(name, owner) # initialized with name and owner
+    @name = name  # name instance variable assigned
+    @owner = owner  # owner instance assigned
+    @mood = 'nervous' # mood set to nervous
+    @@all << self # each instance pushed to class variable array
+  end
+
+  def self.all # getter for @@all class variable
+    @@all
+  end
 end
